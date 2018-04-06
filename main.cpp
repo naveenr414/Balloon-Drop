5f8923135fa393c57ccb0ed525fd0a1f586170e4
naveenr414 <dsfan414@gmail.com>
2018-04-05 22:13:22 -0400

    Used digitalWrite instead of analogWrite

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
-int inputPins[] = {8,7,6,5,4};
+int inputPins[] = {8,9,10,11,12};
 int length = sizeof(inputPins)/sizeof(int);
-int speed = 255;
-int timeDelay = 1000;
+int timeDelay = 300;
 
 void setup()
 {
@@ -25,8 +24,8 @@ void loop()
 	
 	for(int i = 0;i<length;i++)
 	{
-		analogWrite(inputPins[i],speed);
+		digitalWrite(inputPins[i],HIGH);
 		delay(timeDelay);
-		analogWrite(inputPins[i],0);
+		digitalWrite(inputPins[i],LOW);
 	}
 }
